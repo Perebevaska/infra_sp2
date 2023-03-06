a08bc15731cfb9e3a04746bd08272af4a24a6718
Dmitriy Kormin <dmitriykormin@yandex.ru>
2023-03-06 19:32:19 +0500

    1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,11 +7,10 @@ from pathlib import Path
 
 load_dotenv()
 
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
